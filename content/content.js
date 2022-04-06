020e67e5ce6e22837987303ee6a1cb1c4b490d83
Rodrigo da Silva <74737267+Rodrigo-bs@users.noreply.github.com>
2022-04-06 00:53:36 -0300

    Versão funcional da extensão

--- content/content.js
+++ content/content.js
@@ -14,18 +14,38 @@ class InsertPanel {
     createElements() {
         let container = this.createElement('div');
         let button = this.createElement('button');
+        let buttonClose = this.createElement('button');
         let input = this.createElement('input');
 
         container.classList.add('container-get_infos');
-        button.classList.add('button-get_infos');
-        input.classList.add('input-get_infos');
 
-        const finalContainer = this.insertElements(container, [button, input]);
+        button.classList.add('button-get_infos');
+        button.innerText = 'Baixar JSON';
+
+        buttonClose.classList.add('button-close');
+        buttonClose.innerText = '>';
+
+        input.classList.add('input-get_infos');
+        input.setAttribute('type', 'text');
+
+        const finalContainer = this.insertElements(container, [input, button, buttonClose]);
         this.insertInToBody(finalContainer);
+        
+        this.addEvent();
     }
 
     createElement(elementHTML) {
-        return document.createElement(elementHTML.tag);
+        return document.createElement(elementHTML);
+    }
+
+    togglePanel(event) {
+        const container = document.querySelector('.container-get_infos');
+        container.classList.toggle('active');
+    }
+
+    addEvent() {
+        this.buttonClose = document.querySelector('.button-close');
+        this.buttonClose.addEventListener('click', this.togglePanel);
     }
 
     init() {
@@ -33,5 +53,91 @@ class InsertPanel {
     }
 }
 
+class CreateJson {
+    constructor(materia) {
+        this.usersContainer = document.querySelectorAll('.cxdMu');
+        this.messagesContainer = document.querySelectorAll('.GDhqjd');
+        this.data = [];
+
+        this.data.push(materia);
+    }
+
+    getMessages(men, texts) {
+        let dataMen = {
+            name: men.getAttribute('data-sender-name'),
+            message: [],
+            date: men.getAttribute('data-timestamp')
+        };
+
+        texts.forEach(text => {
+            dataMen.message.push(text.innerText);
+        });
+
+        return dataMen
+    }
+
+    getMessage(men) {
+        let dataMen = {
+            name: men.getAttribute('data-sender-name'),
+            message: men.querySelector('[data-message-text]').getAttribute('data-message-text'),
+            date: men.getAttribute('data-timestamp')
+        };
+
+        return dataMen;
+    }
+
+    getUsers() {
+        let data = [];
+
+        this.usersContainer.forEach(user => {
+            let userArray = {
+                name: user.querySelector('.zWGUib').innerText,
+                img: user.querySelector('.KjWwNd').src
+            };
+
+            data.push(userArray);
+        });
+
+        return data;
+    }
+
+    downloadJson(content, fileName, contentType) {
+        var a = document.createElement("a");
+        var file = new Blob([content], { type: contentType });
+        a.href = URL.createObjectURL(file);
+        a.download = fileName;
+        a.click();
+    }
+
+    getDataMessages() {
+        this.messagesContainer.forEach(men => {
+            let messageText = men.querySelectorAll('[data-message-text]');
+
+            console.log(men.querySelectorAll('[data-message-text]').length);
+
+            if (men.querySelectorAll('[data-message-text]').length > 1) this.data.push(this.getMessages(men, messageText));
+            else this.data.push(this.getMessage(men));
+        });
+
+        this.data.push(this.getUsers());
+
+        let time = new Date().getTime();
+        this.downloadJson(JSON.stringify(this.data), time + '.json', 'text/plain');
+    }
+
+    init() {
+        this.getDataMessages();
+    }
+}
+
 const insertPanel = new InsertPanel();
 insertPanel.init();
+
+const button = document.querySelector('.button-get_infos');
+
+button.addEventListener('click', () => {
+    const input = document.querySelector('.input-get_infos');
+
+    const createJson = new CreateJson(input.value);
+    createJson.init();
+});
